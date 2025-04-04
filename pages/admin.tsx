da11709f18f3cbeb00fa7eafd91f6cd9e6a7c22e
2halfone <2halfone@gmail.com>
2025-04-05 00:01:55 +0100

    Fix: adjusted QRCode import (named export)

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import QRCode from 'qrcode.react';
+import { QRCode } from 'qrcode.react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function AdminPage() {
