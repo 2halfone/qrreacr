6e67aa841048688703e0a04ae304a61694e9dc04
2halfone <2halfone@gmail.com>
2025-04-05 00:22:09 +0100

    fix: added type annotations to req and res in submitPresence

--- pages/api/submitPresence.ts
+++ pages/api/submitPresence.ts
@@ -1,6 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { writeToSheets } from './submit-api';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
